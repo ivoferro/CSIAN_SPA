18b8c7f27ebe19bf362b0c8e5376dcb508c9699e
ivoferro <ivoferro1995@gmail.com>
2018-02-18 15:14:11 +0000

    Designs posts collection

--- src/components/posts/PostsCollection.vue
+++ src/components/posts/PostsCollection.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class='col-md-8'>
-    <p>Posts Collection</p>
+  <div class='posts-collection'>
+    <div class='row'>
+      <div class='col-md-7'>
+        <PostCard/>
+      </div>
+      <div class='col-md-5'>
+        <PostCard/>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='col-md-5'>
+        <PostCard/>
+      </div>
+      <div class='col-md-7'>
+        <PostCard/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import PostCard from './PostCard.vue'
+
 export default {
-  name: 'postscollection'
+  name: 'postscollection',
+  components: {
+    PostCard
+  }
 }
 </script>
+
+<style scoped>
+  .posts-collection .row:not(:first-child) {
+    margin-top: 15px;
+  }
+</style>
--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,10 +1,12 @@
 import PostArticle from './PostArticle.vue'
+import PostCard from './PostCard.vue'
 import PostCreation from './PostCreation.vue'
 import PostDetailed from './PostDetailed.vue'
 import PostsCollection from './PostsCollection.vue'
 
 export {
   PostArticle,
+  PostCard,
   PostCreation,
   PostDetailed,
   PostsCollection
